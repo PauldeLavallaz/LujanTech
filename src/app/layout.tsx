4e54d014809d6da5d6f3cad75876db2f11f03eeb
Paul de Lavallaz <pauldelavallaz@hotmail.com>
2024-12-04 18:56:24 -0300

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 		return (
 			<div className="flex h-screen">
 				{isSignedIn && !isPublicRoute && <Sidebar />}
-				<main className="flex-1">
+				<main className="flex-1 flex">
 					{children}
 				</main>
 			</div>
--- src/components/GeneratorLayout.tsx
+++ src/components/GeneratorLayout.tsx
@@ -11,7 +11,7 @@ interface GeneratorLayoutProps {
 
 export function GeneratorLayout({ children, inputs }: GeneratorLayoutProps) {
   return (
-    <div className="flex h-screen">
+    <div className="flex-1 flex">
       {/* Área principal de imágenes generadas */}
       <ScrollArea className="flex-1 p-6">
         <div className="max-w-[1200px] mx-auto">
